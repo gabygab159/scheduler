bc12428fd5ea3c780d01530bb0925c80b574b1be
Gabriel Chen <gabriel.jchen@gmail.com>
2021-04-20 14:15:14 -0400

    more integration testing with application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Appointment />);
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new days is selected", () => {
+    const { getByText } = render(<Application />);
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });
